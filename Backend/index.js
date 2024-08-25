32dcd044c43218ce9dc35c9029f3e3a8c980f128
Gaurav <gauravbishtyt@gmail.com>
2024-08-25 19:47:31 +0530

    d

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,7 @@ connectDB()
         console.log("Connected MongoDB Successfully")
         app.listen(PORT, () => {
             console.log(`Connected with PORT : ${PORT}`)
+            console.log('u')
         })
     })
     .catch((e)=>{
